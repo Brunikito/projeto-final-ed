20861b226e0da60cc94067e93adaa7964006982a
Brunikito <brunoluiszrosa@gmail.com>
2025-06-04 10:35:19 -0300

    merge main

--- src/bst.h
+++ src/bst.h
@@ -1,48 +1,43 @@
-#ifndef BST_H
-#define BST_H
+    #ifndef BST_H
+    #define BST_H
 
-#include <string>
-#include <vector>
-#include <chrono>
+    #include <string>
+    #include <vector>
+    #include <chrono>
 
-namespace BST
-{
+    namespace BST {
 
-	struct Node
-	{
-		std::string word;
-		std::vector<int> documentIds;
-		Node *parent;
-		Node *left;
-		Node *right;
-		int height; // não usado na BST
-		int isRed;	// não usado na BST
-	};
+    struct Node {
+        std::string word;
+        std::vector<int> documentIds;
+        Node* parent;
+        Node* left;
+        Node* right;
+        int height;    // não usado na BST
+        int isRed;     // não usado na BST
+    };
 
-	struct BinaryTree
-	{
-		Node *root;
-		Node *NIL; // não usado na BST
-	};
+    struct BinaryTree {
+        Node* root;
+        Node* NIL; // não usado na BST
+    };
 
-	struct InsertResult
-	{
-		int numComparisons;
-		double executionTime;
-	};
+    struct InsertResult {
+        int numComparisons;
+        double executionTime;
+    };
 
-	struct SearchResult
-	{
-		int found;
-		std::vector<int> documentIds;
-		double executionTime;
-		int numComparisons;
-	};
+    struct SearchResult {
+        int found;
+        std::vector<int> documentIds;
+        double executionTime;
+        int numComparisons;
+    };
 
-	BinaryTree *create();
-	InsertResult insert(BinaryTree *tree, const std::string &word, int documentId);
-	SearchResult search(BinaryTree *tree, const std::string &word);
-	void destroy(BinaryTree *tree);
+    BinaryTree* create();
+    InsertResult insert(BinaryTree* tree, const std::string& word, int documentId);
+    SearchResult search(BinaryTree* tree, const std::string& word);
+    void destroy(BinaryTree* tree);
 
 } // namespace BST
 
