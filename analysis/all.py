cbdcb4e0892a9c74b41ac154d21aa7bdebc40293
arturvidalkrause <avidalkrause@gmail.com>
2025-06-19 01:09:49 -0300

    Graficos completos

--- analysis/all.py
+++ analysis/all.py
@@ -60,8 +60,7 @@ if __name__ == "__main__":
     data_paths = {
         "bst": "../benchmark/results/bst.csv",
         "avl": "../benchmark/results/avl.csv",
-        # comentado pois faltam os dados do rbt
-        # "rbt": "../benchmark/results/rbt.csv",
+        "rbt": "../benchmark/results/rbt.csv",
     }
     
     all_data = {name: pd.read_csv(path) for name, path in data_paths.items()}
